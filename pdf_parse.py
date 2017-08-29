6c0baf18a6236dededbfb54af259bfc3c2723b02
Dipsar <sherawatdevansh@gmail.com>
2017-08-30 05:29:11 +0530

    Database segregated for each student

--- pdf_parse.py
+++ pdf_parse.py
@@ -2,6 +2,7 @@ import PyPDF2
 import sqlite3
 import re
 
+
 pdfres = PyPDF2.PdfFileReader(open('028_ECE_4_SEM.pdf', 'rb'))
 res = pdfres.getPage(3)
 txt = res.extractText()
@@ -22,10 +23,24 @@ txt = res.extractText()
 line = txt.split('\n')
 while '' in line:
     line.remove('')
-line_idx = [i for i, item in enumerate(line) if item.startswith('RTSID:')]
+line_idx = [i for i, item in enumerate(line) if item.startswith('RTSID:')] #finding all the unnecessary data
 
-del line[:line_idx[0]+1]
-print(line)
-    #line.remove(l)
-#for l in line:
-#print(line)
+del line[:line_idx[0]+1] #removing all the unnecessary data
+#finding positions of roll no
+stud = list()
+for l in line:
+    roll_idx = [i for i, item in enumerate(line) if re.search('^\d{11}', item)]
+#dividing student data
+a = 0
+b = 1
+for l in roll_idx:
+    x = roll_idx[a]
+    if a == len(roll_idx)-1 :
+        y = len(line)
+    else :
+        y = roll_idx[b]
+    stud.append(line[x:y])
+    a = a + 1
+    b = b + 1
+for s in stud:
+    print (s)
